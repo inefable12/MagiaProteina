e9fcebfd544265e4ec007d980147879684de5a75
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2026-09-01 22:33:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ st.set_page_config(page_title="Preparador de Proteínas", page_icon="🧬")
 
 ############## PERSONALIZACIÓN DE LA BARRA LATERAL ##############
 try:
-    st.sidebar.image("img/logo.png", caption="Dr. Jesus Alvarado-Huayhuaz")
+    st.sidebar.image("img/logo.png") #, caption="Dr. Jesus Alvarado-Huayhuaz")
+    st.sidebar.write("Dr. Jesus Alvarado-Huayhuaz")
 except Exception:
     pass
 
