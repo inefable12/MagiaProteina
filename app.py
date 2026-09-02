1d093b6d1c7c5f9598b7b60ae29741f64d2b9054
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2026-09-01 21:27:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.sidebar.image("img/logo.png", caption="Dr. Jesus Alvarado-Huayhuaz")
 st.markdown("""
 <style>
     [data-testid="stSidebar"] {
-        background-color: #aabec1;
+        background-color: #711610;
     }
 </style>
 """, unsafe_allow_html=True)
